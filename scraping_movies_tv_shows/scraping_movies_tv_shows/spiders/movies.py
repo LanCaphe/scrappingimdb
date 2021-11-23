487c73ae278269e30a7c142eb82d940688b69e32
ThanhLan <manondoublier@yahoo.fr>
2021-11-23 10:29:45 +0100

    start scrapy project and g 1st spider

---
+++ scraping_movies_tv_shows/scraping_movies_tv_shows/spiders/movies.py
@@ -0,0 +1,20 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class MoviesSpider(CrawlSpider):
+    name = 'movies'
+    allowed_domains = ['https://www.imdb.com/chart/top/?ref_=nv_mv_250']
+    start_urls = ['http://https://www.imdb.com/chart/top/?ref_=nv_mv_250/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        item = {}
+        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
+        #item['name'] = response.xpath('//div[@id="name"]').get()
+        #item['description'] = response.xpath('//div[@id="description"]').get()
+        return item
