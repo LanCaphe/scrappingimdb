8a40df4bfee64ed3fb3a021db2bf9673853af969
ThanhLan <manondoublier@yahoo.fr>
2021-11-24 13:44:09 +0100

    scrapy crawler for movies

--- scraping_movies_tv_shows/scraping_movies_tv_shows/spiders/movies.py
+++ scraping_movies_tv_shows/scraping_movies_tv_shows/spiders/movies.py
@@ -5,16 +5,24 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class MoviesSpider(CrawlSpider):
     name = 'movies'
-    allowed_domains = ['https://www.imdb.com/chart/top/?ref_=nv_mv_250']
-    start_urls = ['http://https://www.imdb.com/chart/top/?ref_=nv_mv_250/']
+    allowed_domains = ['www.imdb.com']
+    start_urls = ['https://www.imdb.com/chart/top/?ref_=nv_mv_250']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    )
+        Rule(LinkExtractor(restrict_xpaths='//td[@class="titleColumn"]/a'), callback = 'parse_item', follow = True),
+        )
 
     def parse_item(self, response):
         item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
+        item['title'] = response.xpath('//h1/text()').get()
+        item['synopsis'] = response.xpath('//span[contains(@class,"GenresAndPlot__TextContainerBreakpointXL-cum89p-2 gCtawA")]/text()').get()
+        item['original title'] = response.xpath('//div[@class="OriginalTitle__OriginalTitleText-jz9bzr-0 llYePj"]/text()').get()
+        item['date'] = response.xpath('//span[@class="TitleBlockMetaData__ListItemText-sc-12ein40-2 jedhex"]/text()').get()
+        item['score'] = response.xpath('//span[@class="AggregateRatingButton__RatingScore-sc-1ll29m0-1 iTLWoV"]/text()').get()
+        item['type'] = response.xpath('//div[@data-testid="genres"]//span/text()').getall()
+        item['actors'] = response.xpath('//a[@class="StyledComponents__ActorName-y9ygcu-1 eyqFnv"]/text()').getall()
+        item['duration'] = response.xpath('//li[@data-testid="title-techspec_runtime"]//div/text()').getall()
+
+      
+
         return item
